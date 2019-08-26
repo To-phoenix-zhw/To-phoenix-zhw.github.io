794f4d701501a89bfb6ff006fe326df9cd84d5ea
zhw <1425619791@qq.com>
2019-08-26 17:09:28 +0800

    Site updated: 2019-08-26 17:09:25

--- js/main.js
+++ js/main.js
@@ -661,4 +661,14 @@ jQuery(document).ready(function($) {
 		Annie_ImageResize();
 		Annie_NiceScroll();
 	})();
+
+$(document).ready(function(){
+    $(document).on('click', '.fold_hider', function(){
+        $('>.fold', this.parentNode).slideToggle();
+        $('>:first', this).toggleClass('open');
+    });
+    //默认情况下折叠
+    $("div.fold").css("display","none");
+});
+
 });
